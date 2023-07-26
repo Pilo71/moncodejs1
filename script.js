fae967531ab0472afe37eb8c8fce341291080fd8
Marvin <cecile.pilorger@gmail.com>
2023-07-26 17:12:01 +0200

    commit 3

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const form = document.querySelector("#form");
+const form = document.querySelector("#envoyer");
 const firstname = document.querySelector("#first-name");
 const lastname = document.querySelector("#last-name");
 const message = document.querySelector("#message");
+document.getElementById("envoyer").addEventListener("submit" ,function (e) {
+    e.preventDefault()
+alert('formulaire envoyé') ;   
+})
