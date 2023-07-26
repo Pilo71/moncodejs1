16cfc16aa085f32ee370cb46482803e068c98adf
Marvin <cecile.pilorger@gmail.com>
2023-07-26 16:18:23 +0200

    branches 2

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("#form");
+const firstname = document.querySelector("#first-name");
+const lastname = document.querySelector("#last-name");
+const message = document.querySelector("#message");
