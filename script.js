6bc46a2c001d9855b6e614d98345b636abfd6d67
Marvin <cecile.pilorger@gmail.com>
2023-07-26 17:46:01 +0200

    commit 4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#envoyer");
 const firstname = document.querySelector("#first-name");
 const lastname = document.querySelector("#last-name");
 const message = document.querySelector("#message");
-document.getElementById("envoyer").addEventListener("submit" ,function (e) {
-    e.preventDefault()
-alert('formulaire envoyé') ;   
-})
+//document.getElementById("envoyer").addEventListener("submit" ,function (e) {
+   // e.preventDefault()
+//alert('formulaire envoyé') ;   
+//})
